5316ced2528e22f003b66317e301ac04d91b9927
unknown <nicky23teague@gmail.com>
2023-04-22 23:14:00 +0100

    v0.1

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// Add this code snippet to the top of your script.js file
+import QrScanner from './qr-scanner.min.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     if (isMobile()) {
       document.getElementById('scan-qr').classList.remove('d-none');
     }
   });
   
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  function isMobile() {
+    // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return true;
   }
-
-document.getElementById('generate-qr').addEventListener('click', () => {
-    const randomBase64 = generateRandomBase64(16); // 16 is the length of the random string
+  
+  document.getElementById('generate-qr').addEventListener('click', () => {
+    const randomBase64 = generateRandomBase64(16);
     const qrElement = document.getElementById('qr-code');
-    
-    // Clear previous QR code if any
     qrElement.innerHTML = '';
   
-    // Create a new QR code with the random base64 string
     const qrCode = new QRCode(qrElement, {
       text: randomBase64,
       width: 256,
@@ -37,38 +36,25 @@ document.getElementById('generate-qr').addEventListener('click', () => {
   
     return result;
   }
-
-  // Replace the previous event listener in your script.js file with this one
-document.getElementById('scan-qr').addEventListener('click', () => {
-    const videoElement = document.createElement('video');
-    videoElement.style.width = '100%';
-    videoElement.id = 'qr-reader'; // Add this line
-    document.body.appendChild(videoElement);
-
+  
+  let qrScanner;
+  
+  document.getElementById('scan-qr').addEventListener('click', () => {
     $('#scan-modal').modal('show');
   
-    const qrScanner = new Html5Qrcode('qr-reader', { fps: 10, qrbox: 250 });
-    
-    qrScanner.start(
-        { facingMode: 'environment' },
-        (decodedText) => {
-          alert('Scanned QR code: ' + decodedText);
-          qrScanner.stop().then(() => {
-            document.body.removeChild(videoElement);
-          });
-        },
-        (error) => {
-          alert('Error: ' + error);
-        }
-      )
-      .catch((error) => {
-        alert('Error: ' + error);
-      });
-  });
-
-  $('#scan-modal').on('hidden.bs.modal', () => {
-    qrScanner.stop().catch((error) => {
-      console.error('Error stopping the scanner:', error);
+    const videoElement = document.getElementById('qr-reader');
+    qrScanner = new QrScanner(videoElement, (decodedText) => {
+      alert('Scanned QR code: ' + decodedText);
+      qrScanner.stop();
+      $('#scan-modal').modal('hide');
     });
+  
+    qrScanner.start();
+  });
+  
+  $('#scan-modal').on('hidden.bs.modal', () => {
+    if (qrScanner) {
+      qrScanner.stop();
+    }
   });
   
