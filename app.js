2441a40e2d87453663fe6365bcccd1d13b73b837
unknown <nicky23teague@gmail.com>
2023-04-27 04:00:43 +0100

    v0.3

--- app.js
+++ app.js
@@ -167,8 +167,8 @@ app.get('/files/:qr_code_id', async (req, res) => {
       const key = crypto.pbkdf2Sync(qr_code_id, salt, 100000, 32, 'sha512');
       
       const decipher = crypto.createDecipheriv(algorithm, key, iv);
-      const decrypted = Buffer.concat([decipher.update(file.file_content), decipher.final()]);
-      
+      const encrypted = Buffer.from(file.file_content, 'hex');  // Convert from hex to Buffer
+      const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
       // Replace the encrypted file content with the decrypted content
       return {
         ...file,
