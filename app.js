4ca25198db9a95bd33076e11c5310fe07f745539
unknown <nicky23teague@gmail.com>
2023-04-27 04:06:28 +0100

    v0.3

--- app.js
+++ app.js
@@ -117,8 +117,8 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     const encrypted = Buffer.concat([cipher.update(file_content), cipher.final()]);
     
     const encryptedFile = {
-      iv: iv.toString('hex'),
-      content: encrypted.toString('hex')
+      iv: iv,
+      content: encrypted
     };
 
     // Now you can store encryptedFile.content in the database, and you need to securely store the key and IV for decryption
@@ -167,8 +167,7 @@ app.get('/files/:qr_code_id', async (req, res) => {
       const key = crypto.pbkdf2Sync(qr_code_id, salt, 100000, 32, 'sha512');
       
       const decipher = crypto.createDecipheriv(algorithm, key, iv);
-      const encrypted = Buffer.from(file.file_content, 'hex');  // Convert from hex to Buffer
-      const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+      const decrypted = Buffer.concat([decipher.update(file.file_content), decipher.final()]);
       // Replace the encrypted file content with the decrypted content
       return {
         ...file,
