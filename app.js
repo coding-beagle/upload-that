2c780a734e29423cafb4f7f7e6a6fef8c74427bb
unknown <nicky23teague@gmail.com>
2023-04-27 04:52:54 +0100

    v0.3

--- app.js
+++ app.js
@@ -165,12 +165,13 @@ app.get('/files/:qr_code_id', async (req, res) => {
       console.log('Retrieved saltHexString:', saltHexString);
 
       const iv = Buffer.from(file.iv, 'hex');
-      console.log('Retrieved IV:', iv);
+      const ivHexString = file.iv.toString('hex');
+      console.log('Retrieved IV:', ivHexString);
 
       // Recreate the encryption key from the qr_code_id and the salt
       const key = crypto.pbkdf2Sync(qr_code_id, saltHexString, 100000, 32, 'sha512');
       
-      const decipher = crypto.createDecipheriv(algorithm, key, iv);
+      const decipher = crypto.createDecipheriv(algorithm, key, ivHexString);
       console.log({key});
       const encrypted = Buffer.from(file.file_content.slice(2), 'hex');  // Convert from hex to Buffer
       const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
